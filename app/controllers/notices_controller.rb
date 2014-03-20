8930c9978a2cab6850cadba9e466aa90b5681280
Aran Cox <arancox@gmail.com>
2014-03-19 20:32:59 -0500

    add a model for notices

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -1,2 +1,11 @@
 class NoticesController < ApplicationController
+  def new
+  end
+  def create
+    @notice = Notice.new(params[:notice])
+ 
+    @notice.save
+    redirect_to @notice
+    #render text: params[:notice].inspect
+  end
 end
